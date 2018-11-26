90075caee6c16fa22d87386757fad1d0ed570a2f
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-25 19:42:17 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,15 +7,37 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+    def edit
+      @author = Author.find(params[:id])
+    end
+
+
+
+  def create
+    @author=Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
   def author_params
-    params.permit(:name, :email, :phone_number)
+    params.permit(:email,:name,:phone_number)
   end
-end
+
+end # end Authors Controller
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100, message: "post must be a least 100 characters"}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
 end
